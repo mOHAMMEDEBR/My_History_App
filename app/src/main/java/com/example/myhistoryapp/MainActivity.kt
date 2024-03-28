9115dc6c72cba890a1a7cb6fd2f7f0836e1df1eb
mOHAMMEDEBR <ebby.kadir@gmail.com>
2024-03-28 16:30:49 +0200

    third commit refactored if statements and text display

--- app/src/main/java/com/example/myhistoryapp/MainActivity.kt
+++ app/src/main/java/com/example/myhistoryapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myhistoryapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,12 +10,13 @@ import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var txtdisage: TextView
-    lateinit var buttonmm: Button
-    lateinit var buttonclear: Button
+    private lateinit var txtdisage: TextView
+    private lateinit var buttonmm: Button
+    private lateinit var buttonclear: Button
 
-    var ageInput: EditText? = null
+    private var ageInput: EditText? = null
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,56 +29,49 @@ class MainActivity : AppCompatActivity() {
 
 
         buttonmm.setOnClickListener {
-            var age = ageInput?.text.toString()
-            txtdisage.setText("$age")
+            val age = ageInput?.text.toString()
+            txtdisage.text = age
 
-            if (age == "39") {
-                txtdisage.setText(
-                    "cleopatra (39) - Last active ruler of the Ptolemaic Kingdom of Egypt."
-                )
-            } else if (age == "55") {
-                txtdisage.setText("Julius Caesar (55) - Roman general and statesman who played a critical role in the events that led to the demise of the Roman Republic"
+            when (age) {
+                "39" -> {
+                    txtdisage.text = "cleopatra (39) - Last active ruler of the Ptolemaic Kingdom of Egypt."
+                }
+                "55" -> {
+                    txtdisage.text = "Julius Caesar (55) - Roman general and statesman who played a critical role in the events that led to the demise of the Roman Republic"
+                }
+                "19" -> {
+                    txtdisage.text = "Joan of Arc (19) - French heroine and military leader during the Hundred Years' War."
 
-                )
-            } else if (age == "19") {
-                 txtdisage.setText(
-                    "Joan of Arc (19) - French heroine and military leader during the Hundred Years' War."
-                )
-
-          } else if (age == "67") {
-                txtdisage.setText(
-                    "Leonardo da Vinci (67) - Italian polymath known for his contributions to art, science, and engineering."
-                )
-            } else if (age == "81") {
-                txtdisage.setText(
-                    "Queen Victoria (81) - Queen of the United Kingdom of Great Britain and Ireland from 1837 to 1901."
-                )
-            } else if (age == "65") {
-                txtdisage.setText(
-                   " Genghis Khan (65) - Founder and first Great Khan of the Mongol Empire, the largest contiguous empire in history"
-                )
-            } else if (age == "88") {
-                txtdisage.setText(
-                    "Michelangelo (88) - Italian sculptor, painter, architect, and poet of the High Renaissance."
-                )
-            } else if (age == "32") {
-                txtdisage.setText( "Anne Boleyn (35) - Queen of England from 1533 to 1536 as the second wife of King Henry VII"
-                )
-            } else if (age == "34") {
-                txtdisage.setText("Wolfgang Amadeus Mozart (35) - Influential composer of the classical era."
-                )
-            } else if (age == "36") {
-                txtdisage.setText(
-                    "Marie Curie (66) - Physicist and chemist known for her pioneering research on radioactivity."
-                )
-            }
-            else {
-                txtdisage.setText("you have selected an age which has no historical people")
+                }
+                "67" -> {
+                    txtdisage.text = "Leonardo da Vinci (67) - Italian polymath known for his contributions to art, science, and engineering."
+                }
+                "81" -> {
+                    txtdisage.text = "Queen Victoria (81) - Queen of the United Kingdom of Great Britain and Ireland from 1837 to 1901."
+                }
+                "65" -> {
+                    txtdisage.text = " Genghis Khan (65) - Founder and first Great Khan of the Mongol Empire, the largest contiguous empire in history"
+                }
+                "88" -> {
+                    txtdisage.text = "Michelangelo (88) - Italian sculptor, painter, architect, and poet of the High Renaissance."
+                }
+                "32" -> {
+                    txtdisage.text = "Anne Boleyn (35) - Queen of England from 1533 to 1536 as the second wife of King Henry VII"
+                }
+                "34" -> {
+                    txtdisage.text = "Wolfgang Amadeus Mozart (35) - Influential composer of the classical era."
+                }
+                "36" -> {
+                    txtdisage.text = "Marie Curie (66) - Physicist and chemist known for her pioneering research on radioactivity."
+                }
+                else -> {
+                    txtdisage.text = "you have selected an age which has no historical people"
+                }
             }
 
             buttonclear.setOnClickListener {
                 ageInput?.text?.clear()
-                txtdisage.setText(" ")
+                txtdisage.text = " "
             }
         }
     }
